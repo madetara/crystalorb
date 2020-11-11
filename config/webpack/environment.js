81dfaf0fc5eb1f30ffdd86affb94251a93569ad4
madetara <x@madetara.dev>
2020-11-11 19:59:17 +0300

    build: add typescript and svelte

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
+const svelte = require('./loaders/svelte')
 
+environment.loaders.prepend('svelte', svelte)
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment
