7dc8a1463a951ee50d2b42be9bfba85f78ef6e7f
madetara <x@madetara.dev>
2020-12-08 01:53:35 +0300

    feat: add devise and basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen-string-literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+end
