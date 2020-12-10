a92ff27b5e80172dd551a976be5aa12fee6652d1
madetara <x@madetara.dev>
2020-12-10 17:10:10 +0300

    refactor: split frontend and backend

--- config/webpack/environment.js
+++
@@ -1,7 +0,0 @@
-const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-const svelte = require('./loaders/svelte')
-
-environment.loaders.prepend('svelte', svelte)
-environment.loaders.prepend('typescript', typescript)
-module.exports = environment
